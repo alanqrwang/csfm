averaging bug

csmpm/model.py
@@ -23,8 +23,10 @@ def generate_measurement(gt, noisy, mask, simulate, a, b, num_captures):
     if simulate:
         assert len(mask) == num_captures, 'Should be Bernoulli mask'
         assert a is not None and b is not None
-        plus = utils.h_plus(gt / num_captures, normalize=False)
-        minus = utils.h_minus(gt / num_captures, normalize=False)
+        # plus = utils.h_plus(gt / num_captures, normalize=False)
+        # minus = utils.h_minus(gt / num_captures, normalize=False)
+        plus = utils.h_plus(gt, normalize=False)
+        minus = utils.h_minus(gt, normalize=False)
         
         plus = plus.unsqueeze(1).repeat(1, num_captures, 1, 1, 1)
         minus = minus.unsqueeze(1).repeat(1, num_captures, 1, 1, 1)
@@ -37,6 +39,15 @@ def generate_measurement(gt, noisy, mask, simulate, a, b, num_captures):
         noisy_minus = torch.poisson(a_inv * minus)
 
         frames = a*noisy_plus - a*noisy_minus + torch.normal(0, std=b) - torch.normal(0, std=b)
+        # frame1 = utils.create_2d_sequency_mask(torch.log(frames[0,0, 0]))
+        # plt.imshow(frame1)
+        # plt.show()
+        # frame2 = utils.create_2d_sequency_mask(torch.log(frames[0,1, 0]))
+        # plt.imshow(frame2)
+        # plt.show()
+        # frame3 = utils.create_2d_sequency_mask(torch.log(frames[0,2, 0]))
+        # plt.imshow(frame3)
+        # plt.show()
 
         y_noisy_under = frames * mask
 
@@ -48,6 +59,12 @@ def generate_measurement(gt, noisy, mask, simulate, a, b, num_captures):
 
         y_noisy_frames = utils.hadamard_transform_torch(noisy.view(-1, nc, n1, n2)).view(noisy.shape)
         y_noisy_under = y_noisy_frames * mask
+        # plt.imshow(noisy[0,0, 0].cpu().detach().numpy())
+        # plt.show()
+        # plt.imshow(noisy[0,1,0].cpu().detach().numpy())
+        # plt.show()
+        # plt.imshow(noisy[0,2,0].cpu().detach().numpy())
+        # plt.show()
 
         y = torch.mean(y_noisy_under, dim=1)
         
@@ -103,6 +120,9 @@ class BaseUnet(nn.Module):
         
 
     def forward(self, clean_img, noisy_img, simulate, a=None, b=None):
+        # plt.imshow(clean_img[0,0].cpu().detach().numpy(), cmap='gray')
+        # plt.show()
+
         mask = self.bernoullimask()
         # mask.register_hook(lambda grad: print('mask', grad))
         y = generate_measurement(clean_img, noisy_img, mask, simulate, a, b, self.num_captures)
@@ -110,8 +130,7 @@ class BaseUnet(nn.Module):
 
         zf = utils.hadamard_transform_torch(y, normalize=False)
         # zf.register_hook(lambda grad: print('inverse h', grad))
-        # plt.imshow(zf[0,0].cpu().detach().numpy())
-        # plt.colorbar()
+        # plt.imshow(zf[0,0].cpu().detach().numpy(), cmap='gray')
         # plt.show()
         # zf = utils.normalize(zf)
         # zf.register_hook(lambda grad: print('zf normalize', grad))

csmpm/solvers.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from . import model, utils
 from pytorch_wavelets import DWTForward, DWTInverse
 import matplotlib.pyplot as plt
+from torch_radon.shearlet import ShearletTransform
 
 class ImageParameter(nn.Module):
     def __init__(self, image_dims, init):
@@ -17,7 +18,7 @@ class ImageParameter(nn.Module):
     def forward(self):
         return self.x
 
-def gradient_descent(y, until_convergence, max_iters, tol, lmbda, lr, device):
+def gradient_descent(y, mask, until_convergence, max_iters, tol, lmbda, lr, device, shearlet):
     '''Gradient descent optimization of regularized regression with TV penalty'''
     loss_list = []
     prev_loss = torch.tensor(0.).float().to(device)
@@ -52,9 +53,10 @@ def gradient_descent(y, until_convergence, max_iters, tol, lmbda, lr, device):
         # plt.show()
 
         # Calculate Loss
-        Ax = utils.hadamard_transform_torch(x, normalize=True)
+        Ax = utils.hadamard_transform_torch(x, normalize=True) * mask
         dc_loss = (1-lmbda)*calc_dc(y, Ax)
-        reg_loss = lmbda*get_tv(x)
+        reg_loss = lmbda*(get_tv(x) + get_wavelets(x, device))
+        # reg_loss = lmbda*(get_tv(x) + get_shearlets(x, shearlet))
         loss = dc_loss + reg_loss
 
         loss_list.append(loss.item())
@@ -115,6 +117,15 @@ def get_wavelets(x, device):
 
     wavelets[:,:,:Yl.shape[-2],:Yl.shape[-1]] = Yl # Put in LL coefficients
     return wavelets.norm(p=1)
+
+def get_shearlets(x, shearlet):
+    # x = x.squeeze()
+    # x = torch.rfft(x, 2, normalized=True, onesided=False)
+    scales = [0.5] * 2
+    # shearlet = ShearletTransform(256, 256, scales)#, cache='/home/aw847/shear_cache/')
+    shears = shearlet.forward(x)
+    l1_shear = shears.norm(p=1)
+    return l1_shear
 
 def nextPowerOf2(n):
     """Get next power of 2"""

csmpm/utils.py
@@ -46,13 +46,8 @@ def h_minus(img, normalize=False):
     else:
         return 1/2 * (torch.ones_like(img)*torch.sum(img) - hadamard_transform_torch(img, normalize=False))
 
-def get_ones_mask(sparsity, shape):
-
-    # base = torch.zeros(n1*n2)
-    # ones = torch.ones(n1*n2 // 2)
-    # base[:n1*n2 // 2] = ones
-    # mask = base.reshape(n1, n2)
-    mask = torch.ones(shape)
+def get_uniform_mask(sparsity, shape):
+    mask = torch.ones(shape) * sparsity
     return mask
 
 def get_equispaced_mask(sparsity, shape):
@@ -71,26 +66,51 @@ def get_random_mask(sparsity, shape):
     prob_mask = torch.bernoulli(p)
     return prob_mask
 
+def get_halfhalf_mask(sparsity):
+    '''Binary mask with 0 and 1 sampled uniformly at random in the 2d mask'''
+    if sparsity == 0.25:
+        prob_mask = np.load('/nfs02/users/aw847/data/fluorescentmicroscopy/masks/halfhalf_4_natural.npy')
+    elif sparsity == 0.125:
+        prob_mask = np.load('/nfs02/users/aw847/data/fluorescentmicroscopy/masks/halfhalf_8_natural.npy')
+    else:
+        raise Exception('No mask')
+    return prob_mask
+
 def normalize(arr):
     """Normalizes a batch of images into range [0, 1]"""
-    if len(arr.shape) > 2:
-        res = torch.zeros_like(arr)
-        for i in range(len(arr)):
-            res[i] = (arr[i] - torch.min(arr[i])) / (torch.max(arr[i]) - torch.min(arr[i]))
-        return res
+    if type(arr) is np.ndarray:
+        if len(arr.shape) > 2:
+            res = np.zeros(arr.shape)
+            for i in range(len(arr)):
+                res[i] = (arr[i] - np.min(arr[i])) / (np.max(arr[i]) - np.min(arr[i]))
+            return res
+        else:
+            return (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
+
     else:
-        return (arr - torch.min(arr)) / (torch.max(arr) - torch.min(arr))
+        if len(arr.shape) > 2:
+            res = torch.zeros_like(arr)
+            for i in range(len(arr)):
+                res[i] = (arr[i] - torch.min(arr[i])) / (torch.max(arr[i]) - torch.min(arr[i]))
+            return res
+        else:
+            return (arr - torch.min(arr)) / (torch.max(arr) - torch.min(arr))
 
 def get_metrics(gt, recons, metric_type, take_avg, normalized=True):
     metrics = []
     if normalized:
-        recons_pro = utils.normalize_recons(recons)
-        gt_pro = utils.normalize_recons(gt)
+        recons_pro = normalize(recons)
+        gt_pro = normalize(gt)
     else:
         recons_pro = myutils.array.make_imshowable(recons)
         gt_pro = myutils.array.make_imshowable(gt)
-    for i in range(len(recons)):
-        metric = myutils.metrics.get_metric(recons_pro[i], gt_pro[i], metric_type)
+
+    if len(recons.shape) > 2:
+        for i in range(len(recons)):
+            metric = myutils.metrics.get_metric(recons_pro[i], gt_pro[i], metric_type)
+            metrics.append(metric)
+    else:
+        metric = myutils.metrics.get_metric(recons_pro, gt_pro, metric_type)
         metrics.append(metric)
 
     if take_avg:
@@ -162,3 +182,23 @@ def add_bool_arg(parser, name, default=True):
     group.add_argument('--' + name, dest=name, action='store_true')
     group.add_argument('--no_' + name, dest=name, action='store_false')
     parser.set_defaults(**{name:default})
+
+def create_2d_sequency_mask(mask):
+    '''2d natural to 2d sequency order'''
+    seq = np.load('/nfs02/users/aw847/data/fluorescentmicroscopy/256x256_2d_seq_indices.npy').astype(int)
+    reordered_mask = np.zeros(mask.shape)
+    for i in range(mask.shape[1] * mask.shape[0]):
+        coord = seq[i,:]
+        reordered_mask[coord[0], coord[1]] = mask.flatten()[i]
+    return reordered_mask
+
+def create_2d_natural_mask(mask):
+    '''2d sequency to 2d natural order'''
+    seq = np.load('/nfs02/users/aw847/data/fluorescentmicroscopy/256x256_2d_seq_indices.npy').astype(int)
+    seq_1d = seq[:,0] * 256 + seq[:,1]
+    had_1d = np.argsort(seq_1d)
+    had_2d = np.unravel_index(had_1d, (256, 256))
+    reordered_mask = np.zeros(mask.shape)
+    for i in range(mask.shape[1] * mask.shape[0]):
+        reordered_mask[had_2d[0][i], had_2d[1][i]] = mask.flatten()[i]
+    return reordered_mask